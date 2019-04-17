f379f6e3f7ecb2f5de70649cf19b65a24ac6bbe7
mrakolice <postevoy.svyatoslav@gmail.com>
2019-04-17 21:19:32 +1000

    add matrix interface

--- Project2/Project2/matrix.c
+++ Project2/Project2/matrix.c
@@ -1,2 +1,29 @@
 
+#include <stdio.h>
+
 #include "matrix.h"
+
+// создаем матрицу, заполненную нулями
+MATRIX_TYPE create_zero_matrix(int n, int m){
+	printf("create_zero_matrix function is UNDEFINED!!!\n");
+}
+
+// создаем матрицу, заполненную косинусом натурального логарифма
+MATRIX_TYPE create_cos_ln_matrix(int n, int m){
+	printf("create_cos_ln_matrix function is UNDEFINED!!!\n");
+}
+
+// Заполняем матрицу косинусами натурального логарифма
+void fill_matrix_with_cos_ln(MATRIX_TYPE matrix){
+	printf("fill_matrix_with_cos_ln function is UNDEFINED!!!\n");
+}
+
+// Удаляем матрицу
+void delete_matrix(MATRIX_TYPE matrix){
+	printf("delete_matrix function is UNDEFINED!!!\n");
+}
+
+// Функция вывода матрицы в консоль
+void output_matrix(int type){
+	printf("output_matrix function is UNDEFINED!!!\n");
+}
--- Project2/Project2/matrix.h
+++ Project2/Project2/matrix.h
@@ -1,6 +1,21 @@
 #ifndef MATRIX_H
 #define MATRIX_H
+	#define MATRIX_TYPE int**
+	#include "output.h"
 
+	// создаем матрицу, заполненную нулями
+	MATRIX_TYPE create_zero_matrix(int n, int m);
 
+	// создаем матрицу, заполненную косинусом натурального логарифма
+	MATRIX_TYPE create_cos_ln_matrix(int n, int m);
+
+	// Заполняем матрицу косинусами натурального логарифма
+	void fill_matrix_with_cos_ln(MATRIX_TYPE matrix);
+
+	// Удаляем матрицу
+	void delete_matrix(MATRIX_TYPE matrix);
+
+	// Функция вывода матрицы в консоль
+	void output_matrix(int type);
 
 #endif
